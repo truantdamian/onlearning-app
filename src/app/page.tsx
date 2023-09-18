0efe095dba646cb6d6d8f407a02e345c3bc19b62
truantd <truantdamian@gmail.com>
2023-09-18 14:51:10 -0300

    prepare primary structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Page() {
-  return <h1>Hello, Next.js!!!</h1>;
+  return (
+    <>
+      <h1>on-learning</h1>
+      <Link href="/student">Quiero Aprender</Link>
+      <Link href="/teacher">Quiero Enseñar</Link>
+    </>
+  );
 }
