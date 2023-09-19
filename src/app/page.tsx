00e167f0898061561620b50b94807ada84a424f2
truantd <truantdamian@gmail.com>
2023-09-19 06:41:24 -0300

    modify layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,15 @@ import Link from "next/link";
 export default function Page() {
   return (
     <>
-      <h1>on-learning</h1>
-      <Link href="/student">Quiero Aprender</Link>
-      <Link href="/teacher">Quiero Enseñar</Link>
+      <header>
+        <h1>on-learning</h1>
+      </header>
+      <main>
+        <div>
+          <Link href="/student">Quiero Aprender</Link>
+          <Link href="/teacher">Quiero Enseñar</Link>
+        </div>
+      </main>
     </>
   );
 }
